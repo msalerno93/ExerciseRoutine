3eb965af50af2ce0a544de327f6f3677bbd3e7fe
msalerno93 <msalerno1993@gmail.com>
2021-05-02 22:10:05 -0400

    fixed login issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,9 +19,9 @@ class ApplicationController < Sinatra::Base
       !!session[:user_id]
     end
 
-    def current_user
-      User.find(session[:user_id])
-    end
+    # def current_user
+    #   User.find(session[:user_id])
+    # end
   end
 
 end
